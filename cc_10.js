87a2cb737e81c4672333ff63ceba0aaa3b84e3a1
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-23 21:35:37 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -28,3 +28,25 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+//Task 2 Order Class
+
+//Creates order class with certain properties
+class Order { 
+    constructor(orderId, product, quantity) { 
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity);
+    };
+
+//Method that retrieves the order details 
+getOrderDetails(){
+    return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+};
+}; 
+
+//Test Cases
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails());
